7eaa12ab0ae57e63126b467da929615817c2597d
Satyapriyo Biswas <satyapriyoak47@gmail.com>
2022-08-14 12:05:04 +0530

    Update resultContextProvider.js

--- src/context/resultContextProvider.js
+++ src/context/resultContextProvider.js
@@ -2,13 +2,12 @@ import React, { createContext, useContext, useState } from "react";
 const resultContext = createContext();
 const baseUrl = "https://google-search1.p.rapidapi.com/google-search";
 
-export const resultContextProvider = ({children})=>{
-    const[results, setResults] = useState([]);
-    const[isLoading, setIsLoading]= useState(false);
-    const[searchItem, setSearchItem]= useState('');
-    const getResults =async(type)=>{
-        setIsLoading(true);
-        const response = await fetch
-    }
-    
-}
+export const resultContextProvider = ({ children }) => {
+  const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchItem, setSearchItem] = useState("");// videos or images or news
+  const getResults = async (type) => {
+    setIsLoading(true);
+    const response = await fetch(`${baseUrl}${type}`);
+  };
+};
