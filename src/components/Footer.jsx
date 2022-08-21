9e3c43654157d971060fd9a7295364003eb8ccff
Satyapriyo Biswas <satyapriyoak47@gmail.com>
2022-08-21 21:31:08 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     <>
     <div className=" fixed items-center text-center bottom-0 h-[10%] bg-slate-400 w-[100%] dark:bg-slate-800">
       <div className='px-4'>ভারত</div>
-      <div className='bg-slate-300 h-[100%] p-0 text-xl'>Google 2022 &copy; , Inc</div>
+      <div className='bg-slate-300 h-[100%] p-0 text-xl'>Google 2022 &copy; . Inc</div>
     </div>
     </>
   )
