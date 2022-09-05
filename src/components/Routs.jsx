fecdb2ae04206def0af6e1a7f28c3671d282e1f5
Satyapriyo Biswas <satyapriyoak47@gmail.com>
2022-09-05 21:13:34 +0530

    updating routes

--- src/components/Routs.jsx
+++ src/components/Routs.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Navbar from "./Navbar"
 import Search from './Search'
 import Results from './Results'
 const Routs = () => {
   return (
     <Routes>
-      <Route element={<Navbar/>}></Route>
-      <Route element={<Search/>}></Route>
-      <Route element={<Results/>}></Route>
+      <Route path="/" >
+        <Search/>
+        </Route>
+      <Route path="/search" element={<Search/>}></Route>
+      <Route path={["/images" , "/search","/news","/videos"]}>
+        <Results/>
+      </Route>
+      <Route  element={<Results/>}></Route>
     </Routes>
   );
 }
